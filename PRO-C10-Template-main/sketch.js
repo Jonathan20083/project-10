b2e7ee0e91a3bb7b58799c857871503a90b2a229
Jonathan20083 <103178313+Jonathan20083@users.noreply.github.com>
2022-05-09 19:14:46 +0930

    Add files via upload

--- PRO-C10-Template-main/sketch.js
+++ PRO-C10-Template-main/sketch.js
@@ -6,9 +6,9 @@ function preload(){
 
   shipImg1 = loadAnimation("ship-1.png");
   shipImg1 = loadAnimation("ship-1.png");
-  shipImg1 = loadAnimation("ship-1");
+  shipImg1 = loadAnimation("ship-1.png");
   shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-1.png","ship-2.png");
-  shipImg1 = loadAnimation("ship-1","ship-2","ship-1","ship-2");
+  shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-1.png","ship-2.png");
   
   seaImg = loadImage("sea.png");
 }
@@ -25,7 +25,7 @@ function setup(){
 
   
   ship = createSprite(130,200,30,30);
-  ship.addAnimation("movingShip",shipImg1);
+  ship.addAnimation("movingShip.png",shipImg1);
   ship.scale =0.25;
   
 }
@@ -35,11 +35,12 @@ function draw() {
   sea.velocityX = -3;
 
   //uncomment code to reset the background
-  if(sea.x < 0){
+  if(sea.x <= 0){
     sea.x = 0;
     sea.x = sea.width;
     sea.x = sea.width/8;
-    sea.y = height;
+   
+    
   }
 
  
